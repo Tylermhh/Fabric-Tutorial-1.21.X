2f2d4b21049273279b2acfbdaede5ad1cd4fee93
Tylermhh <tyler.hlaing@gmail.com>
2025-05-08 12:12:28 -0700

    Initial commit

---
+++ src/main/java/net/tyler/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.tyler.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
