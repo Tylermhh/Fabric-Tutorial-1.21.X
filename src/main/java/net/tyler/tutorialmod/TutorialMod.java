254ca9f0809493786113f2a097eff52b732e31ee
Tylermhh <tyler.hlaing@gmail.com>
2025-05-08 12:38:08 -0700

    added new pink garnet item into the game

--- src/main/java/net/tyler/tutorialmod/TutorialMod.java
+++ src/main/java/net/tyler/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.tyler.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tyler.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +16,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
